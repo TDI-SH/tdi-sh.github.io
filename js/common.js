e539050e8e4aebe4051c048af8691bc814bcb3e3
X-Y-1223 <ningxiaoyu@tagadmindeiMac-2.local>
2016-06-08 15:31:40 +0800

    update

--- js/common.js
+++ js/common.js
@@ -9,10 +9,14 @@
     var menuOpen=document.querySelector('.menu__open');
     
     btnMenuOpen.addEventListener('click',handleMenuOpen,false);
-    btnMenuOpen.addEventListener('touchstart',handleMenuOpen,false);
+    //btnMenuOpen.addEventListener('touchstart',handleMenuOpen,false);
     
     btnMenuClose.addEventListener('click',handleMenuClose,false);
+<<<<<<< Updated upstream
     //btnMenuClose.addEventListener('touchstart click',handleMenuClose,false);
+=======
+    //btnMenuClose.addEventListener('touchstart',handleMenuClose,false);
+>>>>>>> Stashed changes
 
     
     function handleMenuOpen(e)
