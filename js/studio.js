26c4dc39db6f71df0eb748e2ccdf0530e270d56b
robberfree <RWU@CNSGHMD008.local>
2016-06-29 17:08:22 +0800

    test

--- js/studio.js
+++ js/studio.js
@@ -2,36 +2,30 @@
 {
     console.log('studio.js start');    
     
-    var studioCards=document.querySelectorAll('.studio__card');
-    var studioCardOvers=document.querySelectorAll('.studio__card_over');
-    var num=studioCards.length;
+    var studioRight=document.querySelector('.studio__right');   
+    var cardDetails=document.querySelector('.studio__card__details');
+    
     var btnOurWorks=document.querySelector('.studio__intro__ourworks');
     var btnLeft=document.querySelector('.studio__btnLeft');
+    var btnCloseDetail=document.querySelector('.studio__card__detail__btnClose');     
+    
     var studioIntro=document.querySelector('.studio>.studio__left');
-    var studioOurWorks=document.querySelector('.studio__ourworks'); 
+    var studioOurWorks=document.querySelector('.studio__ourworks');
     
     addClickEvent(btnLeft,leaveOurWorks);
     addClickEvent(btnOurWorks,enterOurWorks);
+    addClickEvent(btnCloseDetail,closeCardDetails);
+    studioRight.addEventListener('mouseover',handleOverCards,false);
+    studioRight.addEventListener('mouseout',handleOutCards,false);
+    addClickEvent(studioRight,handleClickCards);   
     window.addEventListener('resize',handleResize,false);
     
-    
-    handleResize(null)
-    
-    
-    function handleResize(e)
-    {
-        var studioCardWidth=getComputedStyle(studioCards[0]).width;
-        var studioCardOverWidth=getComputedStyle(studioCardOvers[0]).width;
-        for(var i=0;i<num;i++)
-        {
-            studioCards[i].style.height=studioCardWidth;
-            //studioCardOvers[i].style.height=studioCardOverWidth;
-        }               
-    }
+    handleResize(null);  
     
     function enterOurWorks(e)
-    {
-        TweenLite.to(studioIntro,0.5,{alpha:0,left:'-32%'});
+    {        
+        e.preventDefault();
+        TweenLite.to(studioIntro,0.5,{alpha:0});
         
         studioOurWorks.style.display='block';
         TweenLite.set(studioOurWorks,{alpha:0});
@@ -40,16 +34,96 @@
     
     function leaveOurWorks(e)
     {
+        e.preventDefault();
         TweenLite.set(studioIntro,{alpha:0});
         TweenLite.to(studioIntro,0.5,{alpha:1});       
         
-        TweenLite.to(studioOurWorks,0.5,{alpha:0,onComplete:function(){studioOurWorks.style.display='none';}});        
+        TweenLite.to(studioOurWorks,0.5,{alpha:0,onComplete:function(){studioOurWorks.style.display='none';closeCardDetails(null)}});        
     }
+    
+    var lastCardWidth=-1;
+    function handleResize(e)
+    {
+        if(e!==null)
+            e.preventDefault();
+        var isMobile=false;
+        var windowWidth=window.innerWidth;
+        if(windowWidth<480)
+            isMobile=true;
+        if(windowWidth<960)
+            windowWidth=960;
+        var studioCardWidth=windowWidth*0.68*0.5-10;
+        if(studioCardWidth!==lastCardWidth&&!isMobile)
+        {
+            var cards=document.querySelectorAll('.studio__card');
+            var num=cards.length;           
+            for(var i=0;i<num;i++)
+            {
+                var card=cards[i];
+                card.style.width=card.style.height=studioCardWidth+'px';
+            } 
+            lastCardWidth=studioCardWidth;       
+        }                      
+    }
+    
+    function handleOverCards(e)
+    {
+        e.preventDefault();
+        var target=e.target;
+        if(target.classList.contains('studio__card'))
+        {
+             var card=target;
+             var cardOver=card.querySelector('.studio__card_over');
+             cardOver.classList.add('studio__card_over_show');         
+        }       
+    }
+    
+    function handleOutCards(e)
+    {
+        e.preventDefault();
+        var target=e.target;
+        if(target.classList.contains('studio__card'))
+        {
+             var card=target;
+             var cardOver=card.querySelector('.studio__card_over');
+             cardOver.classList.remove('studio__card_over_show');         
+        }       
+    }
+    
+    function handleClickCards(e)
+    {
+        e.preventDefault();
+        var target=e.target;
+        if(target.classList.contains('studio__card'))
+        {             
+             var card=target;
+             var cards=card.parentNode.querySelectorAll('.studio__card');
+             var index=Array.prototype.indexOf.call(cards, card);//获得子div在父div中的索引
+             
+             cardDetails.style.display='block';
+             
+             var cardDetail=document.querySelectorAll('.studio__card__detail')[index];
+                 cardDetail.style.display='block';            
+        }
+    }
+    
+    function closeCardDetails(e)
+    {
+        if(e!==null)
+            e.preventDefault();
+        cardDetails.style.display='none';
+        
+        Array.prototype.forEach.call(document.querySelectorAll('.studio__card__detail'), function(el, i){
+            el.style.display='none';
+        });      
+    }
+    
+    
     
     function addClickEvent(dom,handler)
     {
         dom.addEventListener('click',handler,false);
-        dom.addEventListener('touchstart',handler,false);        
+        dom.addEventListener('touchend',handler,false);        
     }
     
     
