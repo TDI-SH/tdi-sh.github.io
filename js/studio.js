fa4106ffb2d80ecb57c188768df07fc23a36e1e9
robberfree <RWU@CNSGHMD008.local>
2016-08-02 16:22:41 +0800

    update

--- js/studio.js
+++ js/studio.js
@@ -13,6 +13,9 @@
     var studioIntro=document.querySelector('.studio>.studio__left');
     var studioOurWorks=document.querySelector('.studio__ourworks');
     
+    var strEl=createStrElement('Transcreation','font-family:Helvetica;font-weight:100;font-size:4.6rem;'); 
+
+    
     addClickEvent(btnLeft,leaveOurWorks);
     addClickEvent(btnOurWorks,enterOurWorks);
     addClickEvent(btnCloseDetail,closeCardDetails);
@@ -62,8 +65,9 @@
         
         TweenLite.to(studioOurWorks,0.5,{alpha:0,onComplete:function(){studioOurWorks.style.display='none';closeCardDetails(null);}});        
     }
-    
-    var isMobile;
+
+   
+    var isMobile;    
     function handleResize(e)
     {
         if(e!==null)
@@ -71,27 +75,72 @@
                  
         var windowWidth=window.innerWidth;
         if(windowWidth<=480)
+        {
             isMobile=true;
+            Array.prototype.forEach.call(document.querySelectorAll('.studio__card'), function(el, i){
+                    el.style.width=el.style.height=windowWidth+'px';
+            });
+        }
         else
         {
-            isMobile=false;
-            
-            if(windowWidth<960)
-                windowWidth=960;
+            isMobile=false;            
             var studioCardWidth=windowWidth*0.68*0.5-10;
-            var cards=document.querySelectorAll('.studio__card');
-            var num=cards.length;           
-            for(var i=0;i<num;i++)
+            Array.prototype.forEach.call(document.querySelectorAll('.studio__card'), function(el, i){
+                    el.style.width=el.style.height=studioCardWidth+'px';
+            });
+            
+        }
+
+        //根据.studio__left的padding和width计算.studio__left>h2
+        var finding=true;
+        var padding=getNumber(getComputedStyle(studioIntro).paddingLeft);
+        var introWidth=getNumber(getComputedStyle(studioIntro).width);           
+
+        while(finding)
+        {
+            
+            var fontSize=getNumber(getComputedStyle(strEl).fontSize);            
+            var strElWidth=getNumber(getComputedStyle(strEl).width);        
+            var offsetWidth=introWidth-padding*2-strElWidth;
+            if(offsetWidth<-10)
             {
-                var card=cards[i];
-                card.style.width=card.style.height=studioCardWidth+'px';
+                fontSize-=2;
+                strEl.style.fontSize=fontSize+'px';
             }
-        }              
+            else if(offsetWidth>10)
+            {
+                fontSize+=2;
+                strEl.style.fontSize=fontSize+'px';
+            }
+            else
+            {                
+                finding=false;  
+                fontSize=fontSize>72?72:fontSize;              
+                Array.prototype.forEach.call(document.querySelectorAll('.studio__left>h2'), function(el, i){
+                    el.style.fontSize=fontSize+'px';
+                });
+            }              
+        }                
+    }
+
+    function createStrElement(str,fontStyle)
+    {        
+        var strEl=document.createElement('pre');    
+        fontStyle+='display:inline-block;visibility:hidden;';            
+        strEl.innerText=str;
+        strEl.style.cssText=fontStyle;        
+        document.body.appendChild(strEl);
+        return strEl;              
+    }
+
+    function getNumber(str)
+    {
+        return Number(str.substring(0,str.lastIndexOf('px')));
     }
     
     function handleOverCards(e)
     {
-        console.log(e.type);
+        //console.log(e.type);
         if(e.type!=='touchstart')//mobile端不要禁止studioRight上的touchstart行为
             e.preventDefault();
         var target=e.target;
@@ -105,7 +154,7 @@
     
     function handleOutCards(e)
     {
-        console.log(e.target,e.type);
+        //console.log(e.target,e.type);
         if(e.type!='touchend')//mobile端不要禁止studioRight上的touchend行为
             e.preventDefault();
         var target=e.target;
@@ -119,7 +168,7 @@
     
     function handleClickCards(e)
     {
-        console.log(e.target,e.type)
+        //console.log(e.target,e.type)
         //mobile端touchstart和mouseover都会触发卡片的over效果，所以强制调用一次handleOutCards,清除over效果
         handleOutCards(e);
                 
