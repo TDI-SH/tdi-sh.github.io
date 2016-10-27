6e4423068626bfd5b332872dc31d540e028df768
robberfree <RWU@CNSHAMD0008.local>
2016-10-27 10:24:12 +0800

    upload

--- upload/main.js
+++ upload/main.js
@@ -1,22 +1,24 @@
 (function () {
     //var canvas = document.querySelector('canvas');
     //var ctx = canvas.getContext('2d');
-    var img=document.querySelector('img');
+    var img = document.querySelector('img');
+    var infoUpload = document.querySelector('.info-upload');
 
     var input = document.querySelector('input[type=file]');
     input.addEventListener('change', handleSelectFile, false);
 
     function handleSelectFile(e) {
-        console.log('select file');
+        updateInfo('start select file', infoUpload);
 
         var file = input.files[0];
-        console.log(file);
 
         var fr = new FileReader();
         fr.addEventListener('load', loadFileComplete, false);
+        fr.addEventListener('error', loadError, false)
 
 
         function loadFileComplete(e) {
+            updateInfo('FileReader load file complete', infoUpload);
             console.log('load complete');
             //var img = new Image();
             img.src = e.target.result;
@@ -26,7 +28,17 @@
             //ctx.drawImage(img, 0, 0);
         }
 
+        function loadError(e) {
+            updateInfo('FileReader read file error' + e.target.error.name, infoUpload);
+        }
+
         fr.readAsDataURL(file);
+    }
+
+    function updateInfo(msg, info) {
+        var oldMsg = info.innerHTML;
+        var msg = oldMsg + '<br/>' + msg;
+        info.innerHTML = msg;
     }
 
 
